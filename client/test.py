2d44ad98f9525e48f982d6f2d352ca56d421d711
beauxq <beauxq@users.noreply.github.com>
2017-10-20 11:40:40 -0500

    refactor main test

--- client/test.py
+++ client/test.py
@@ -1,20 +1,29 @@
 from physicalInterface import SimPhysicalInterface
 from environmentSimulator import EnvironmentSimulator
 
-a = SimPhysicalInterface()
-print(a.env.text_map())
-a.forward()
-print("read_sensor:", a.read_sensor())
-a.forward()
-print("read_sensor:", a.read_sensor())
-a.rotate_left()
-a.forward()
-print("read_sensor:", a.read_sensor())
-a.forward()
-print("read_sensor:", a.read_sensor())
 
-"""
-b = EnvironmentSimulator()
-b.generate(4, 4)
-print(b.text_map())
-"""
+def test_sim_physical_interface():
+    a = SimPhysicalInterface()
+    print(a.env.text_map())
+    a.forward()
+    print("read_sensor:", a.read_sensor())
+    a.forward()
+    print("read_sensor:", a.read_sensor())
+    a.rotate_left()
+    a.forward()
+    print("read_sensor:", a.read_sensor())
+    a.forward()
+    print("read_sensor:", a.read_sensor())
+
+
+def test_environment_simulator():
+    b = EnvironmentSimulator()
+    b.generate(4, 4)
+    print(b.text_map())
+
+
+def main():
+    test_sim_physical_interface()
+
+if __name__ == "__main__":
+    main()
