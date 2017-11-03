2d9401d2724be777534941f6b0e476a7abb201da
beauxq <beauxq@users.noreply.github.com>
2017-11-03 13:10:23 -0500

    start into sockets

--- client/comm/CommunicationManager.py
+++ client/comm/CommunicationManager.py
@@ -3,9 +3,13 @@ from comm import Message
 from utils import Coordinate
 
 from queue import Queue
+import socket
 
 
 class CommunicationManager:
+    HOST = ""  # all available interfaces
+    PORT = 7676
+
     def __init__(self, data_repository: DataRepository):
         self._data = data_repository
         self._incoming_messages = Queue()  # TODO: parameter is max length, decide on a good one
@@ -18,8 +22,27 @@ class CommunicationManager:
 
     def _receive_incoming_messages(self):
         """ open listening socket and put messages in queue """
-        # TODO: open listening socket and put messages in queue
-        pass
+        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            print("starting listening socket")
+            listen_socket.bind((CommunicationManager.HOST, CommunicationManager.PORT))
+        except Exception as e:
+            print("unable to start listening socket", e)
+            return
+        print("listening socket opened")
+
+        while True:
+            listen_socket.listen()
+
+            connection, address = listen_socket.accept()  # this line blocks the thread until a connection comes
+            print("incoming connection from", address)
+
+            data = connection.recv(1024)  # receive data from client
+            string = bytes.decode(data)  # decode it to string
+            print("message:", string)
+            self._incoming_messages.put(Message(string))
+
+            connection.close()
 
     def send_message(self, message: Message):
         pass
